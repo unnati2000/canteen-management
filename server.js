40c7b8c9bb5d4c30f3c19a496e222b75ef2731b8
unnati2000 <unnatibamania8@gmail.com>
2020-10-06 10:52:32 +0530

    finally

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
+const path = require('path');
 
 app.use(express.json({ extended: false }));
 connectDB();
@@ -19,6 +20,12 @@ app.use(
   })
 );
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
   res.send('done!');
