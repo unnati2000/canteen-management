8883a8417854ffd756563c240dcfb07c0fd36ef9
unnati2000 <unnatibamania8@gmail.com>
2020-10-06 23:49:59 +0530

    Nitin is dumb

--- server.js
+++ server.js
@@ -12,16 +12,16 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
+app.use(require('./Routes/order'));
+app.use(require('./Routes/auth'));
+app.use(require('./Routes/food'));
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-
-app.use(require('./Routes/order'));
-app.use(require('./Routes/auth'));
-app.use(require('./Routes/food'));
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
