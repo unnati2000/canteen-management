6b5204076df99cf4b0cb40442b7d1620ee3f5e3a
unnati2000 <unnatibamania8@gmail.com>
2020-10-06 20:24:42 +0530

    Switch position of route imports

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ connectDB();
 
 app.use(cors());
 
-app.use(require('./Routes/order'));
-app.use(require('./Routes/auth'));
-app.use(require('./Routes/food'));
-
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'production') {
@@ -22,6 +18,10 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
+app.use(require('./Routes/order'));
+app.use(require('./Routes/auth'));
+app.use(require('./Routes/food'));
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
